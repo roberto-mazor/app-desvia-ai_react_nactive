e22ca342015d13123f7f42f5f30a8fdd104db08e
roberto-mazor <mazor.estudo@gmail.com>
2026-08-18 01:53:47 -0300

    Create AuthController.ts

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,18 @@
+import { UserModel } from "@/models/UserModel";
+import { IUser } from "@/types";
+
+export const AuthController = {
+    login: async (email: string, password: string): Promise<IUser> => {
+        if (!email || !password) throw new Error('Preencha e-mail e senha.');
+        const users = await UserModel.getUsers();
+        const user = users.find(u => u.email === email && u.password === password);
+        if (!user) throw new Error('E-mail ou senha incorretos.');
+        return user;
+    },
+    register: async (name: string, email: string, password: string): Promise<IUser> => {
+        if (!name || !email || !password) throw new Error('Preencha todos os campos.');
+        const newUser: IUser = { name, email, password};
+        await UserModel.saveUser(newUser);
+        return newUser;
+    }
+}
